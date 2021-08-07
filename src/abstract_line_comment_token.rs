bd30043472f5e2100e1245dd79a3bc0cc25d2f4c
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-07 15:34:24 +0800

    Add abstract line comment token

---
+++ src/abstract_line_comment_token.rs
@@ -0,0 +1,8 @@
+/// An abstraction of line comment token.
+pub trait AbstractLineCommentToken {
+	/// The line in line comment token which could be a string, array of bytes, or other type.
+	type Line;
+
+	/// Returns the line in line comment token.
+	fn line(&self) -> Self::Line;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod token_kind;
 mod abstract_token;
+mod abstract_line_comment_token;
 
 pub use token_kind::TokenKind;
 pub use abstract_token::AbstractToken;
+pub use abstract_line_comment_token::AbstractLineCommentToken;
