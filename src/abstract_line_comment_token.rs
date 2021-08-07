3537e60abd17eb025ba380f55c14da27cf39b44e
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-07 20:16:41 +0800

    Add methods to create token from general token

--- src/abstract_line_comment_token.rs
+++ src/abstract_line_comment_token.rs
@@ -1,3 +1,5 @@
+use crate::AbstractToken;
+
 /// An abstraction of line comment token.
 pub trait AbstractLineCommentToken {
 	/// The line in line comment token which could be a string, array of bytes, or other type.
@@ -5,4 +7,7 @@ pub trait AbstractLineCommentToken {
 
 	/// Returns the line in line comment token.
 	fn line(&self) -> Self::Line;
+
+	/// Creates line comment token.
+	fn from_token<T: AbstractToken>(_: T) -> Self;
 }
--- src/abstract_scope_level_token.rs
+++ src/abstract_scope_level_token.rs
@@ -1,5 +1,10 @@
+use crate::AbstractToken;
+
 /// An abstraction of scope level token.
 pub trait AbstractScopeLevelToken {
 	/// Returns the scope level it contains.
 	fn level(&self) -> usize;
+
+	/// Creates scope level token.
+	fn from_token<T: AbstractToken>(_: T) -> Self;
 }
