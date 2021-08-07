5b2b0abeb0be9d949833dc8a8c12831d0f9d0e2e
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-07 23:22:49 +0800

    Improve the documentation of trait for converting from general token

--- src/from_token.rs
+++ src/from_token.rs
@@ -1,7 +1,7 @@
 use crate::AbstractToken;
 
-/// Allows general Chearmyp token to converted into more specific token.
+/// Allows general Chearmyp token to converted into specific token.
 pub trait FromToken {
-	/// Converts the general token into more specific token.
+	/// Converts the general token into specific token.
 	fn from_token<T: AbstractToken>(_: T) -> Self;
 }
