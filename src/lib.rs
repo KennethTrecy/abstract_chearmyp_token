853ccf772ee26b6e05d11e57a0b4cb3dd933d716
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-08 15:25:24 +0800

    Update documentation in the crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(feature = "no_std", no_std)]
 
-//! # Chearmyp Abstract Token
+//! # Abstract Chearmyp Token
 //! Please read the README.md for more information.
 //!
 //! ## Features available
