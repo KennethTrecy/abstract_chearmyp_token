5ce86013ebf0bf12c8c848327654034f220a58c2
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-16 21:57:24 +0800

    Use generics for customizable input types

--- src/abstract_token.rs
+++ src/abstract_token.rs
@@ -1,40 +1,38 @@
-use crate::abstracts::{AbstractSource, AbstractSourceCollection};
+use crate::abstracts::{AbstractBoundary, AbstractBoundaryCollection};
 use crate::TokenKind;
 
 /// An abstraction of Chearmyp token.
-pub trait AbstractToken {
-	/// The type of source where some tokens will be built from.
-	type Source: AbstractSource;
-
-	/// The type of source collection where some tokens will be built from.
-	type SourceCollection: AbstractSourceCollection;
-
+pub trait AbstractToken<T, U, V, W, X>
+where
+	U: AbstractBoundary<T>,
+	W: AbstractBoundary<V>,
+	X: AbstractBoundaryCollection<V, W> {
 	/// Returns the kind of the token it holds.
 	fn kind(&self) -> TokenKind;
 
 	/// Creates new line comment token.
-	fn new_line_comment(_: Self::Source) -> Self;
+	fn new_line_comment(_: U) -> Self;
 
 	/// Creates new scope level token.
 	fn new_scope_level(_: usize) -> Self;
 
 	/// Creates new simplex token.
-	fn new_simplex(_: Self::Source) -> Self;
+	fn new_simplex(_: U) -> Self;
 
 	/// Creates new line othertongue token.
-	fn new_line_othertongue(_: Self::Source) -> Self;
+	fn new_line_othertongue(_: U) -> Self;
 
 	/// Creates new block comment token.
-	fn new_block_comment(_: Self::SourceCollection) -> Self;
+	fn new_block_comment(_: X) -> Self;
 
 	/// Creates new complex token.
-	fn new_complex(_: Self::Source) -> Self;
+	fn new_complex(_: U) -> Self;
 
 	/// Creates new attacher token.
 	///
 	/// First parameter is the label, and the second parameter is the content.
-	fn new_attacher(_: Self::Source, _: Self::Source) -> Self;
+	fn new_attacher(_: U, _: U) -> Self;
 
 	/// Creates new block othertongue token.
-	fn new_block_othertongue(_: Self::SourceCollection) -> Self;
+	fn new_block_othertongue(_: X) -> Self;
 }
--- src/abstract_token_queue.rs
+++ src/abstract_token_queue.rs
@@ -1,18 +1,18 @@
+use crate::abstracts::{AbstractBoundary, AbstractBoundaryCollection};
 use crate::AbstractToken;
 
 /// An abstraction of Chearmyp token queue.
-pub trait AbstractTokenQueue {
-	/// The type of token that the queue contains.
-	type Token: AbstractToken;
-
-	/// Creates an empty token queue.
-	fn new() -> Self;
-
+pub trait AbstractTokenQueue<T, U, V, W, X, Y>
+where
+	U: AbstractBoundary<T>,
+	W: AbstractBoundary<V>,
+	X: AbstractBoundaryCollection<V, W>,
+	Y: AbstractToken<T, U, V, W, X> {
 	/// Puts the token into the token queue.
-	fn push_token(&mut self, _: Self::Token);
+	fn push_token(&mut self, _: Y);
 
 	/// Takes the first token in the token queue.
-	fn shift_token(&mut self) -> Option<Self::Token>;
+	fn shift_token(&mut self) -> Option<Y>;
 }
 
 #[cfg(feature = "vecdeque_token_queue")]
