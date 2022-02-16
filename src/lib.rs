9b0f2192acc5ad5e17bf5ab00c3da94c68116ed2
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-16 22:16:38 +0800

    Update the root of the library

--- src/lib.rs
+++ src/lib.rs
@@ -21,17 +21,36 @@
 extern crate alloc;
 
 mod abstracts {
-	pub use abstract_chearmyp_source::{AbstractSource, AbstractSourceCollection};
+	pub use abstract_chearmyp_boundary::{AbstractBoundary, AbstractBoundaryCollection};
+}
+
+#[cfg(test)]
+mod native {
+	#[cfg(feature = "no_std")]
+	pub use core::ops::Range;
+
+	#[cfg(feature = "no_std")]
+	pub use alloc::vec::Vec;
+
+	#[cfg(not(feature = "no_std"))]
+	pub use std::{
+		vec::Vec,
+		ops::Range
+	};
 }
 
 mod token_kind;
 mod abstract_token;
 mod abstract_token_queue;
+mod simple_abstract_token;
+mod dynamic_abstract_token;
 mod specific_abstract_tokens;
 
 pub use token_kind::TokenKind;
 pub use abstract_token::AbstractToken;
 pub use abstract_token_queue::AbstractTokenQueue;
+pub use simple_abstract_token::SimpleAbstractToken;
+pub use dynamic_abstract_token::DynamicAbstractToken;
 pub use specific_abstract_tokens::{
 	AbstractComplexToken,
 	AbstractSimplexToken,
