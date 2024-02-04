7fb933316ec35a7a2f98b95e19e57c578664cc3c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-02-04 13:15:58 +0800

    feat!: renew the token kinds

--- src/token_kind.rs
+++ src/token_kind.rs
@@ -1,12 +1,10 @@
 /// Possible kinds of Chearmyp tokens.
 #[cfg_attr(feature = "assertable_token_kind", derive(Debug, PartialEq))]
 pub enum TokenKind {
-	Simplex,
-	Complex,
+	Edon,
 	Attacher,
 	ScopeLevel,
 	BlockComment,
 	LineComment,
-	LineOthertongue,
-	BlockOthertongue
+	Othertongue
 }
